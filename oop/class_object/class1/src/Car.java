d1c05f0e6098ccce3d7d564d6af515d5f61e79d5
EvgenyAndrus <evg_an@mail.ru>
2022-07-07 14:14:40 +0300

    class_object->start

---
+++ oop/class_object/class1/src/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    private int speed;
+    private String color;
+    public Car(int speed,String color){
+        this.speed = speed;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Speed is:%d\nColor is:%s",speed,color);
+    }
+}
---
+++ oop/class_object/class1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(160,"blue");
+        System.out.println(car.getClass());
+        System.out.println(car);
+    }
+}
